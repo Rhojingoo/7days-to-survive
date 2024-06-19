fc2a339710e18dbf2078dce495e9937512badf95
민승 김 <rlaalstmd0606@gmail.com>
2024-06-19 14:51:38 +0900

    김민승 : 컨트롤러 입력 매핑 옮김

--- Source/Sevendays_to_survive/Player/MainController/C_MainPlayerController.h
+++ Source/Sevendays_to_survive/Player/MainController/C_MainPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "C_MainPlayerController.generated.h"
 
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +14,16 @@ UCLASS()
 class SEVENDAYS_TO_SURVIVE_API AC_MainPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+protected:
+
+	/** Input Mapping Context to be used for player input */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* InputMappingContext;
+
+	// Begin Actor interface
+protected:
+
+	virtual void BeginPlay() override;
+
+	// End Actor interface
 };
