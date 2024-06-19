25d61a18608442390cee7fbaf4bb5f9d13fb6f7d
seungjae94 이승재 <mathlife12345@gmail.com>
2024-06-19 18:27:38 +0900

    이승재: 건축물 구조 변경

--- Source/Sevendays_to_survive/BuildingSystem/C_BuildingPartInterface.cpp
+++ Source/Sevendays_to_survive/BuildingSystem/C_BuildingPartInterface.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BuildingSystem/BuildingPart.h"
+#include "BuildingSystem/C_BuildingPartInterface.h"
 
 // Add default functionality here for any IBuildingPart functions that are not pure virtual.
