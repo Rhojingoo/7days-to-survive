c3b40f26eed0d085e3f3c260e7a49592b8322aab
usm <AssortRock0@AssortRock>
2024-06-24 15:29:19 +0900

    유성민

---
+++ Source/Sevendays_to_survive/Monster/MonsterAI/MonsterTask/C_TaskMonsterChase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "C_TaskMonsterChase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEVENDAYS_TO_SURVIVE_API UC_TaskMonsterChase : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
