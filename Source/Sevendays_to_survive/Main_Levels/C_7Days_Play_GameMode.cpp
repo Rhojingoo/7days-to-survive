e2838dab63baf7398833d303ac9cee87eb67cc61
rhojingoo <rhojingoo@naver.com>
2024-06-25 14:54:33 +0900

    노진구 :: 메인 게임모드 추가

---
+++ Source/Sevendays_to_survive/Main_Levels/C_7Days_Play_GameMode.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Main_Levels/C_7Days_Play_GameMode.h"
+
---
+++ Source/Sevendays_to_survive/Main_Levels/C_7Days_Play_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "C_7Days_Play_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEVENDAYS_TO_SURVIVE_API AC_7Days_Play_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
