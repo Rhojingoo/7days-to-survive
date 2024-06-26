81799aafd45b48863a1b3d8323425232fe1dfe3a
usm <AssortRock0@AssortRock>
2024-06-26 18:57:48 +0900

    유성민 : 문제 수정

--- Source/Sevendays_to_survive/Monster/MonsterAI/MonsterTask/C_TaskMonsterPatrol.cpp
+++ Source/Sevendays_to_survive/Monster/MonsterAI/MonsterTask/C_TaskMonsterPatrol.cpp
@@ -8,6 +8,7 @@
 #include "Monster/MonsterAI/C_MonsterAIBase.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Navigation/PathFollowingComponent.h"
+#include "Monster/C_ZombieBase.h"
 
 UC_TaskMonsterPatrol::UC_TaskMonsterPatrol()
 {
@@ -22,35 +23,38 @@ void UC_TaskMonsterPatrol::InitTask(UBehaviorTreeComponent* OwnerComp)
 
 EBTNodeResult::Type UC_TaskMonsterPatrol::PatrolMove(AC_MonsterAIBase* _MonsterController)
 {
-	//FVector Origin = FVector(_MonsterController->GetPawn()->GetActorLocation()); // 자기 자신의 위치 (예: 현재 Actor의 위치)
-	//float Radius = 1000.0f; // 반경
-	//RandomVector = GetRandomVectorInRadius(Origin, Radius);
-	//UE_LOG(LogTemp, Log, TEXT("Random Vector in Radius: %s"), *RandomVector.ToString());
+	FVector Origin = FVector(_MonsterController->GetPawn()->GetActorLocation()); // 자기 자신의 위치 (예: 현재 Actor의 위치)
+	float Radius = 1000.0f; // 반경
+	UBlackboardComponent* Blackboard = _MonsterController->GetBlackboardComponent();
+	Blackboard->SetValueAsVector(*RandomVector, GetRandomVectorInRadius(Origin, Radius));
+	UE_LOG(LogTemp, Log, TEXT("Random Vector in Radius: %s"), *RandomVector);
 
-	//EPathFollowingRequestResult::Type MoveResult = _MonsterController->MoveToLocation(RandomVector);
-	//if (MoveResult == EPathFollowingRequestResult::Failed)
-	//{
-	//	return EBTNodeResult::Failed;
-	//}
+	EPathFollowingRequestResult::Type MoveResult = _MonsterController->MoveToLocation(Blackboard->GetValueAsVector(*RandomVector));
+	if (MoveResult == EPathFollowingRequestResult::Failed)
+	{
+		return EBTNodeResult::Failed;
+	}
 	return EBTNodeResult::InProgress;
 }
 
 EBTNodeResult::Type UC_TaskMonsterPatrol::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	Super::ExecuteTask(OwnerComp, NodeMemory);
-
-	if (TaskController->GetIsFind() == true) {
+	AC_MonsterAIBase* Controller = GetController(&OwnerComp);
+	UBlackboardComponent* BlackBoard = GetBlackBoard(&OwnerComp);
+	if (Controller->GetIsFind() == true) {
 		return EBTNodeResult::Succeeded;
 	}
-
-	if (!IsValid(TaskController)) {
+	UObject* object = (BlackBoard->GetValueAsObject("SelfActor")); //디버깅용
+	AC_ZombieBase* Zom = Cast<AC_ZombieBase>(object);
+	if (!IsValid(Controller)) {
 		UE_LOG(LogTemp, Warning, TEXT("MonsterController is Not Work BTTESK %d  %s"), __LINE__, ANSI_TO_TCHAR(__FUNCTION__));
 		return EBTNodeResult::Failed;
 	}
 
 	else {
-		BlackboardComp->SetValueAsBool(*BoolName, false);
-		return PatrolMove(TaskController);
+		BlackBoard->SetValueAsBool(*BoolName, false);
+		return PatrolMove(Controller);
 	}
 	//return EBTNodeResult::Failed;
 }
@@ -58,13 +62,13 @@ EBTNodeResult::Type UC_TaskMonsterPatrol::ExecuteTask(UBehaviorTreeComponent& Ow
 void UC_TaskMonsterPatrol::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
-
-	//if (TaskController->GetIsFind() || TaskController->GetMoveStatus() != EPathFollowingStatus::Moving) {
-	//	TaskController->GetBlackboardComponent()->SetValueAsBool(*BoolName, true);
-	//	RandomVector = FVector::ZeroVector;
-	//	FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
-	//	return;
-	//}
+	AC_MonsterAIBase* Controller = GetController(&OwnerComp);
+	UBlackboardComponent* BlackBoard = GetBlackBoard(&OwnerComp);
+	if (Controller->GetIsFind() || Controller->GetMoveStatus() != EPathFollowingStatus::Moving) {
+		BlackBoard->SetValueAsBool(*BoolName, true);
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return;
+	}
 }
 
 // 주어진 반경 내에서 랜덤 벡터를 반환하는 함수
