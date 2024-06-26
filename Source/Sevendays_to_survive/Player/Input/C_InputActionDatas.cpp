c662f63246181055af93a555cf625b7605251987
민승 김 <rlaalstmd0606@gmail.com>
2024-06-26 18:16:07 +0900

    김민승 : 입력 데이터 에셋 정리중

---
+++ Source/Sevendays_to_survive/Player/Input/C_InputActionDatas.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Input/C_InputActionDatas.h"
+
+UC_InputActionDatas::UC_InputActionDatas()
+{
+}
+
+UC_InputActionDatas::~UC_InputActionDatas()
+{
+}
+
