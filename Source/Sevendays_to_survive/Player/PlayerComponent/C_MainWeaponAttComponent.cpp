041bbc6dc9fd369b480480db5485f2b099091551
민승 김 <rlaalstmd0606@gmail.com>
2024-06-28 15:01:40 +0900

    김민승 : 검, 소총 애니메이션 동기화(O)

---
+++ Source/Sevendays_to_survive/Player/PlayerComponent/C_MainWeaponAttComponent.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/PlayerComponent/C_MainWeaponAttComponent.h"
+#include "Player/Global/C_GlobalPlayer.h"
+
+// Sets default values for this component's properties
+UC_MainWeaponAttComponent::UC_MainWeaponAttComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+void UC_MainWeaponAttComponent::AttStart_Implementation()
+{
+	if (false == IsAttCPP)
+	{
+		IsAttCPP = true;
+		PlayerMesh->GetAnimInstance()->Montage_Play(AttAni);
+		IsAttCPP = false;
+	}
+}
+
+// Called when the game starts
+void UC_MainWeaponAttComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+	Player = GetOwner<AC_GlobalPlayer>();
+	PlayerMesh = Player->GetMesh();
+}
+
+
+// Called every frame
+void UC_MainWeaponAttComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
