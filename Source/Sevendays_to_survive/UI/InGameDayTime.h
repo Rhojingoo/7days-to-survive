2c173174accbcd75d70cb414737ca78949b6870f
cine <earth_to_us@naver.com>
2024-07-03 18:50:17 +0900

    안시내 ; 현재날짜시간 업데이트, 크래프트 목록 업데이트

---
+++ Source/Sevendays_to_survive/UI/InGameDayTime.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameDayTime.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEVENDAYS_TO_SURVIVE_API UInGameDayTime : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
