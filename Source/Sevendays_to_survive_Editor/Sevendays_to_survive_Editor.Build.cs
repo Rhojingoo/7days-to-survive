ad4f887aa6769c69c2dcea34aeffed6c6ee87881
KMJ <mjya8928@naver.com>
2024-07-04 18:43:03 +0900

    240704 김명지 : 에디터 모듈 생성 에디터 위젯(MyWidget) 생성

---
+++ Source/Sevendays_to_survive_Editor/Sevendays_to_survive_Editor.Build.cs
@@ -0,0 +1,27 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System.IO;
+using UnrealBuildTool;
+using UnrealBuildTool.Rules;
+
+public class Sevendays_to_survive_Editor : ModuleRules
+{
+	public Sevendays_to_survive_Editor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "Landscape", "UMG", "DesktopPlatform" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, ""));
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
+}
---
+++ Source/Sevendays_to_survive_Editor/Sevendays_to_survive_Editor.cpp
@@ -0,0 +1,8 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sevendays_to_survive_Editor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SevenDays_Editor_Log, "Sevendays_to_survive_Editor" );
+
+DEFINE_LOG_CATEGORY(SevenDays_Editor_Log);
