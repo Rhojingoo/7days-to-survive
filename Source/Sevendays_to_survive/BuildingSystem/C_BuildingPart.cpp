39bb34884d94162199aa3999c13a614869073572
seungjae94 이승재 <mathlife12345@gmail.com>
2024-07-04 18:15:38 +0900

    이승재: 건축물 TakeDamage 함수 구현

--- Source/Sevendays_to_survive/BuildingSystem/C_BuildingPart.cpp
+++ Source/Sevendays_to_survive/BuildingSystem/C_BuildingPart.cpp
@@ -1,5 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "BuildingSystem/C_BuildingPart.h"
 
+#include "Net/UnrealNetwork.h"
+
+AC_BuildingPart::AC_BuildingPart()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    bReplicates = true;
+}
+
+void AC_BuildingPart::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(AC_BuildingPart, Hp);
+}
+
+float AC_BuildingPart::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+    ReceiveDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+    return DamageAmount;
+}
+
+void AC_BuildingPart::ReceiveDamage_Implementation(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+    Hp -= DamageAmount;
+
+    if (Hp <= 0.0f)
+    {
+        Destroy();
+    }
+}
