11c3e314fc7a96494ecdf11c2ae0499fa1ed9b95
seungjae94 이승재 <mathlife12345@gmail.com>
2024-07-08 14:23:51 +0900

    이승재: RangedZombie cpp 추가

---
+++ Source/Sevendays_to_survive/Monster/C_RangedZombie.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monster/C_RangedZombie.h"
+
+AC_RangedZombie::AC_RangedZombie()
+{
+    SetName("RangedZombie");
+}
+
+void AC_RangedZombie::Shoot()
+{
+
+}
+
+void AC_RangedZombie::Attack_Implementation()
+{
+
+}
+
+void AC_RangedZombie::RunAttack_Implementation()
+{
+
+}
+
+void AC_RangedZombie::SetName(FString _Name)
+{
+    MonsterName = _Name;
+}
