e728b2b600be1611cf6b212fc5ddb887c2e2fbaa
seungjae94 이승재 <mathlife12345@gmail.com>
2024-07-10 16:36:45 +0900

    이승재: MapActor 구현

---
+++ Source/Sevendays_to_survive/Map/C_MapActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Map/C_MapActor.h"
+
+// Sets default values
+AC_MapActor::AC_MapActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AC_MapActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AC_MapActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
