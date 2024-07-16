870e0cb1c47ffd7dc3166645cf59e16735f536ae
rhojingoo <rhojingoo@naver.com>
2024-07-16 10:32:15 +0900

    노진구 :: 인스턴스에 플레이어 도착 알림 불값지정

--- Source/Sevendays_to_survive/STS/C_STSInstance.h
+++ Source/Sevendays_to_survive/STS/C_STSInstance.h
@@ -48,27 +48,31 @@ public:
 
 public:
 	// 인게임 밤낮 구분변수
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DauorNight", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DayorNight", meta = (AllowPrivateAccess = "true"))
 	bool IsDay;
 
 	// 인게임 날자카운트
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DauorNight", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DayorNight", meta = (AllowPrivateAccess = "true"))
 	int DayCount;
 
 	// 시간UI를 띄우기 위해 계산하는 시간 
 	// ★★★12기준으로 시간적용(밤낮 변경)★★★
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DauorNight", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DayorNight", meta = (AllowPrivateAccess = "true"))
 	int Time;
 
 	// 실제 인게임에서 UI시간
 	// ★★★ 06시 ~ 17시 == 낮,  18시 ~ 05시 == 밤  ★★★
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DauorNight", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DayorNight", meta = (AllowPrivateAccess = "true"))
 	int GameUI_Time;
 
 	// 인게임에서 엔딩 알림
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DauorNight", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DayorNight", meta = (AllowPrivateAccess = "true"))
 	bool Game_End;
 
+	// 플레이어 도착 알림
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DayorNight", meta = (AllowPrivateAccess = "true"))
+	bool Player_arrive;
+	
 
 	UFUNCTION(BlueprintCallable)
 	void SetSpawnMonster();
