ecedf0630e39b290af4a44a45c5e8decffa0c84e
YHJ <yhjin07@naver.com>
2024-07-23 17:39:34 +0900

    진연호 : 스폰 지점 배치

--- Source/Sevendays_to_survive/Map/C_MonsterSpawnPoint.cpp
+++ Source/Sevendays_to_survive/Map/C_MonsterSpawnPoint.cpp
@@ -22,7 +22,7 @@ AC_MonsterSpawnPoint::AC_MonsterSpawnPoint()
 	CollisionCheckBoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionCheckBoxComponent"));
 	CollisionCheckBoxComponent->SetupAttachment(RootComponent);
 	//CollisionCheckBoxComponent->SetHiddenInGame(false);
-	CollisionCheckBoxComponent->SetBoxExtent({ 200.0f, 200.0f, 400.0f });
+	CollisionCheckBoxComponent->SetBoxExtent({ 200.0f, 100.0f, 600.0f });
 
 }
 
