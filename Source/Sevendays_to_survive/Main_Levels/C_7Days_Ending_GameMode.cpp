45cbced93c1b79d1fe3c42d498458f8e6d7f3358
seungjae94 이승재 <mathlife12345@gmail.com>
2024-07-26 18:51:16 +0900

    이승재: 커밋 테스트

--- Source/Sevendays_to_survive/Main_Levels/C_7Days_Ending_GameMode.cpp
+++ Source/Sevendays_to_survive/Main_Levels/C_7Days_Ending_GameMode.cpp
@@ -1,5 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Main_Levels/C_7Days_Ending_GameMode.h"
 
+//Test
